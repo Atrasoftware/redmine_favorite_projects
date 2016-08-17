ad1290040d5a104e5605b84b452e88f511fca899
bilel-kedidi <bilel.kedidi@gmail.com>
2016-08-17 21:12:54 +0100

    add context menu

--- config/routes.rb
+++ config/routes.rb
@@ -8,3 +8,8 @@ match "favorite_projects/unfavorite" => "favorite_projects#unfavorite", :via => 
 match "favorite_projects/search" => "favorite_projects#search", :as => "search_favorite_projects", :via => [:get, :post]
 
 match 'auto_completes/project_tags' => 'auto_completes#project_tags', :via => :get, :as => 'auto_complete_project_tags'
+
+get 'context_menus/project', :to => 'context_menus#project_context_menu', :as => 'project_context_menu'
+get 'context_menus/update_project_cf', :to => 'context_menus#update_project_cf' , :as => 'update_project_cf'
+post 'context_menus/update_project_cf', :to => 'context_menus#update_project_cf'
+
