4cf0ed2eb9cf255d5826dfd9836d74e2dcc16f2d
bilel <bilel.kedidi@gmail.com>
2017-09-14 15:54:10 +0100

    fix project sorting

--- lib/redmine_favorite_projects/patches/issue_query_patch.rb
+++ lib/redmine_favorite_projects/patches/issue_query_patch.rb
@@ -20,6 +20,8 @@ module RedmineFavoriteProjects
       def available_columns_with_project
         return @available_columns if @available_columns
         @available_columns = available_columns_without_project
+        match =  @available_columns.find{|c| c.name == :project}
+        @available_columns[@available_columns.index(match)] = QueryColumn.new(:project, :sortable => "#{Project.table_name}.identifier_with_cfs", :groupable => true)
         @available_columns += project_custom_fields.collect {|cf| QueryCustomFieldColumn.new(cf) }
         @available_columns
       end
